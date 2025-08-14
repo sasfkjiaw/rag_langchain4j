52288a6bbbb21629800d8e6c6a5ce183b9a87d08
sasfkjiaw <1635272368@qq.com>
2025-08-14 17:42:59 +0800

    七、项目实战-创建硅谷小智

---
+++ src/main/java/com/zzz/rag/langchain4j/assistant/XiaozhiAgent.java
@@ -0,0 +1,20 @@
+package com.zzz.rag.langchain4j.assistant;
+
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+
+import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
+
+@AiService(
+        wiringMode = EXPLICIT,
+        chatModel = "qwenChatModel",
+        chatMemoryProvider = "chatMemoryProviderXiaozhi"
+)
+public interface XiaozhiAgent {
+
+    @SystemMessage(fromResource = "xiaozhi-prompt-template.txt")
+    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+
+}
